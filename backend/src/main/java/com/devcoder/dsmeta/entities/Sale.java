8f37ee4e232fbd531abafff4dc2b133508204ded
Jonas Santos <jonasrodrigod11@gmail.com>
2022-11-19 10:46:00 -0300

    Database

---
+++ backend/src/main/java/com/devcoder/dsmeta/entities/Sale.java
@@ -0,0 +1,68 @@
+package com.devcoder.dsmeta.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity // equivale a classe a uma tabela do banco
+@Table(name = "tb_sales") 
+public class Sale {
+	
+	@Id // identifica cada um dos dados
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // gera o numero do Id automaticamente
+	private Long id;
+	private String sellerName;
+	private Integer visited;
+	private Integer deals;
+	private  LocalDate date;
+	
+	public Sale()  {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSellerName() {
+		return sellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+	
+	
+}
