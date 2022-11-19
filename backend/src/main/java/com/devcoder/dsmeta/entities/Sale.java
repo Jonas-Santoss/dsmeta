a0aff74c5c43c4f95ff65e90e869c229cbf40c43
Jonas Santos <jonasrodrigod11@gmail.com>
2022-11-19 11:16:03 -0300

    Database

--- backend/src/main/java/com/devcoder/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/devcoder/dsmeta/entities/Sale.java
@@ -18,51 +18,42 @@ public class Sale {
 	private String sellerName;
 	private Integer visited;
 	private Integer deals;
+	private Double amount;
 	private  LocalDate date;
-	
-	public Sale()  {
-	}
-
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getSellerName() {
 		return sellerName;
 	}
-
 	public void setSellerName(String sellerName) {
 		this.sellerName = sellerName;
 	}
-
 	public Integer getVisited() {
 		return visited;
 	}
-
 	public void setVisited(Integer visited) {
 		this.visited = visited;
 	}
-
 	public Integer getDeals() {
 		return deals;
 	}
-
 	public void setDeals(Integer deals) {
 		this.deals = deals;
 	}
-
+	public Double getAmount() {
+		return amount;
+	}
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
 	public LocalDate getDate() {
 		return date;
 	}
-
 	public void setDate(LocalDate date) {
 		this.date = date;
 	}
-	
-	
-	
 }
